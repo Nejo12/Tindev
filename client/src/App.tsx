ce163430f6009578c2287151015f31e4ec53f3ae
Gabriel <olaniyi_g@hotmail.com>
2020-12-14 21:53:23 +0100

    version conflict: Bug fixed

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => (
+  <div className="App">
+    <p>Welcome to TinDev</p>
+    <p>Team 4 ;) </p>
+    <p>test</p>
+  </div>
+)
+
+export default App
