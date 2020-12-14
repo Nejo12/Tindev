ed249bd59fd312c3a068564e006816f6b6ba90a7
Gabriel <olaniyi_g@hotmail.com>
2020-12-14 20:21:53 +0100

    pulled feature-frontend

--- client/src/redux/saga/index.ts
+++
@@ -1,6 +0,0 @@
-import { all, fork } from 'redux-saga/effects'
-import { watchNewGeneratedNumberRequestStart } from './numberfile'
-
-export const rootSaga = function* root() {
-  yield all([fork(watchNewGeneratedNumberRequestStart)])
-}
