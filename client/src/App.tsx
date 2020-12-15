f35b3398bd01671149a92aace6ac19fe7f0cf34d
Gabriel <olaniyi_g@hotmail.com>
2020-12-15 13:46:08 +0100

    Frontend test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import 'semantic-ui-css/semantic.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container } from 'semantic-ui-react'
+import './sass/styles.scss'
 
 import Header from './components/Header'
+import NavBar from './components/NavBar'
 
 const App = () => (
   <div>
+    <NavBar />
     <Header />
     <Container textAlign="center">
       <h1>Welcome to TinDev</h1>
--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -7,12 +7,14 @@ const Header = () => {
       <Menu.Item style={{ fontSize: 24 }}>
         <Icon name="cogs" color="red"></Icon> TinDev
       </Menu.Item>
+
       <Menu.Item position="right">
         <Button inverted color="green">
           <Icon name="sign-in"></Icon>
           Login
         </Button>
       </Menu.Item>
+
       <Menu.Item>
         <Button inverted color="violet">
           <Icon name="add"></Icon>
